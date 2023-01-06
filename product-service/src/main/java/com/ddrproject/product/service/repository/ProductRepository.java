b73a49c4339743bfdb1c99e99f8ec8714a3f1bd7
Dadader <ar444555666777@gmail.com>
2023-01-06 19:15:19 +0500

    product service

---
+++ product-service/src/main/java/com/ddrproject/product/service/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.ddrproject.product.service.repository;
+
+import com.ddrproject.product.service.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product,String> {
+}
